4e0b366076ce4ea9a527f286386e291a5b44f795
TheDynamicVector <63025401+TheDynamicVector@users.noreply.github.com>
2023-09-16 12:05:10 -0400

    Godot Implementation

---
+++ Global.gd
@@ -0,0 +1,17 @@
+extends Node
+
+var player_1_score = 0
+var player_2_score = 0
+
+var player_1_gesture = ""
+var player_2_gesture = ""
+
+var game_started = true
+
+func _process(delta):
+	
+	if game_started:
+		var gestures = []
+		OS.execute("python", ["/handrecognition.py"], gestures)
+		
+		player_1_gesture = gestures[0]
